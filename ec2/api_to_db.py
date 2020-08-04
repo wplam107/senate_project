441be1847e99c7dcf3718502170be6e789cab39c
Wayne <w.p.lam107@gmail.com>
2020-08-04 12:56:22 -0400

    update script and notebook

--- ec2/api_to_db.py
+++ ec2/api_to_db.py
@@ -3,6 +3,7 @@ import json
 import configparser
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from datetime import datetime
 
 # Set up connection to AWS RDS
 config1 = configparser.ConfigParser()
@@ -57,9 +58,7 @@ if __name__ == '__main__':
     most_recent = (votes[0]['congress'], votes[0]['session'], votes[0]['roll_call'])
 
     # Determine if there are new votes
-    if most_recent == last_bill:
-        update = False
-    else:
+    if most_recent != last_bill:
         update = True
         rcs_to_pull = []
         for vote in votes:
@@ -83,7 +82,8 @@ if __name__ == '__main__':
 
         # Convert results to dictionaries to be inserted into database
         bill_to_db = []
-        votes = []
+        new_votes = []
+        new_sens = []
         for bill in list_of_bills:
             item = bill['votes']['vote']
             if item['bill'] != {}: # Ignore nominations
@@ -95,29 +95,108 @@ if __name__ == '__main__':
                     roll_call = item['roll_call']
                     date = item
                     csr_id = f'{congress}.{session}.{roll_call}'
-                    bill_dict = {
-                        'csr_id': csr_id,
-                        'congress': congress,
-                        'session': session,
-                        'roll_call': roll_call,
-                        'bill_id': item['bill']['bill_id'],
-                        'date': vote['date']
-                    }
-                    bill_to_db.append(bill_dict)
+                    bill_tup = (
+                        csr_id,
+                        congress,
+                        session,
+                        roll_call,
+                        item['bill']['bill_id'],
+                        vote['date']
+                    )
+                    bill_to_db.append(bill_tup)
 
                     for position in item['positions']:
-                        vote_dict = {
-                            'sen_id': position['member_id'],
-                            'csr_id': csr_id,
-                            'position': position['vote_position']
-                        }
-                        votes.append(vote_dict)
+                        vote_tup = (
+                            position['member_id'],
+                            csr_id,
+                            position['vote_position']
+                        )
+                        new_votes.append(vote_tup)
+                        if position['member_id'] not in sen_ids:
+                            new_sens.append(position['member_id'])
+    else:
+        update = False
+        list_of_bills = []
+        new_votes = []
+
+    # Get newest list of senators from latest bill
+    if len(list_of_bills) != 0:
+        current_congress = [ mem['member_id'] for mem in list_of_bills[0]['votes']['vote']['positions'] ]
+    else:
+        current_congress = []
+
+    # Compare current members to database members and add new member
+    members_to_add = []
+    for member in current_congress:
+        if member not in sen_ids:
+            members_to_add.append(member)
+
+    # Get senator data from API
+    sen_dicts = []
+    for mem in members_to_add:
+        r = requests.get(
+                f'https://api.propublica.org/congress/v1/members/{mem}.json',
+                headers={'X-API-Key': api_key}
+            )
+        results = r.json()['results']
+
+    # Using function from scraping notebook
+    def get_senators(members):
+        senators = []
+        for member in members:
+            senator = (
+                member['id'],
+                member['first_name'],
+                member['last_name'],
+                member['party'],
+                member['gender'],
+                member['state'],
+            )
+            senators.append(senator)
+        return senators
+
+    new_sens = get_senators(sen_dicts)
+    if len(new_sens) != 0:
+        update_sen = True
+    else:
+        update_sen = False
+
+    # Prepare queries
+    add_senator = (
+        """
+        INSERT INTO senators (sen_id, f_name, l_name, party, gender, state)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """
+    )
+
+    add_bill = (
+        """
+        INSERT INTO bills (csr_id, congress, session, roll_call, bill_id, date)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """
+    )
+
+    add_vote = (
+        """
+        INSERT INTO votes (sen_id, csr_id, position)
+        VALUES (%s, %s, %s);
+        """
+    )
 
     # Insert bills and votes into database
     if update == True:
-        for vote in votes:
-
+        # Must add bills first as it is a FK for votes
+        cursor.executemany(add_bill, bill_to_db)
+        cursor.executemany(add_vote, new_votes)
+        if update_sen == True:
+            cursor.executemany(add_senator, new_sens)
 
     cursor.close()
     conn.close()
+
+    # Write update date to file
+    f = open('recent_update.txt', 'a')
+    f.write(f'Date: {datetime.date(datetime.now())}, Update: {update}, New Senators: {update_sen}\n')
+    f.close()
+
     
